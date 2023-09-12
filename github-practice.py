9429798b798f146abb97a5a8305bfe174e57b6b2
nexer nul <redpenguin4luca26@gmail.com>
2023-09-12 00:22:03 +0000

    completed delete/add function todolist

--- github-practice.py
+++ github-practice.py
@@ -1,16 +1,36 @@
 toDoList = ["Math Homework", "Cook Dinner", "Fold Laundry"]
 
-def addItem(item):
-    toDoList.append(userAns)
+def addItem(userAns):
+    toDoList.append(itemA)
     return toDoList
 
-userAns = input("Do you want to add to your list or quit? A/Q")
-while userAns == "A":
-   item = input("What item do you want to add?")
-   addItem(item)
-   userAns = input("Do you want to add to your list or quit? A/Q")
-   while userAns == "Q":
-    print(toDoList)
-    return
+def deleteItem(userEdit):
+    toDoList.remove(itemD)
+    return toDoList
+
+while True:
+    userAns = input("Do you want to edit to your list or quit? E/Q") 
+    if userAns == 'Q':
+            print(toDoList)
+            break
+    elif userAns == 'E':
+            userEdit = input("Would you like to add or delete from list? A/D")
+            if userEdit == 'A':
+                itemA = input("What would you like to add?")
+                addItem(itemA)
+                print(toDoList)
+                
+            elif userEdit == 'D':
+                    itemD = input("Which would you like to delete?")
+                    if itemD in toDoList:
+                        deleteItem(itemD)                      
+                        print("Item successfuly deleted")
+                        #else: print(ITEM NOT FOUND)
+                        
+                        
+                        
+    
+
+
 
         
